dae8b43aeb53a0cfa5d4f0448e6894fa7ba0e75e
Julia Gonzalez <julia.gonzalez98@gmail.com>
2023-03-15 20:11:05 -0300

    clases

--- appjulia/models.py
+++ appjulia/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 
 # Create your models here.
-class Cliente(models.Model):
+class Usuario(models.Model):
     nombre = models.CharField(max_length=15)
     apellido = models.CharField(max_length=15)
     DNI = models.CharField(max_length=15)
+    
+class Actividad(models.Model):
+    actividad_elegida = models.CharField(max_length=35)
+    clase = models.CharField(max_length=1)
+
+class Reserva(models.Model):
+     turno = models.CharField(max_length=5)
+
+
     
